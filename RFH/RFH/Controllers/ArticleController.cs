8f932ff9a1640e7f539c3d7e298a8013fe5ad18a
Stephen Walther <Stephen.Walther@superexpert.com>
2011-10-22 14:37:17 -0700

    Added Article controller and view

---
+++ RFH/RFH/Controllers/ArticleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RFH.Infrastructure;
+using System.Data.Entity;
+
+namespace RFH.Controllers
+{
+    public class ArticleController : Controller
+    {
+
+        private DataContext _dataContext = new DataContext();
+
+        public ActionResult Index(int id)
+        {
+            // Lookup the article by id (only include published articles)
+            var article = (from a in _dataContext.Articles.Include(a => a.HostSite).Include(a=>a.Category)
+                           where a.Id == id && a.IsPublished == true
+                           select a).FirstOrDefault();
+
+            // If you cannot find the article then redirect to Home
+            if (article == null) {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(article);
+        }
+
+    }
+}
