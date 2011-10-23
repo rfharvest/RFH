692862a44795e7f7c07455f26523e99a404d105a
naraen <naraen@gmail.com>
2011-10-23 10:06:09 -0700

    Issue #3.  Adding CKEditor to the more forms

--- RFH/RFH/Infrastructure/EditorHelper.cs
+++ RFH/RFH/Infrastructure/EditorHelper.cs
@@ -8,15 +8,15 @@ using System.Text;
 namespace RFH.Infrastructure {
     public static class EditorHelper {
 
-//        const string tag = @"
-//   <textarea name=""{0}"" id=""{0}"">{1}</textarea>
-//        <script type=""text/javascript"">CKEDITOR.replace('{0}');</script>";
+        const string tag = @"
+            <textarea name=""{0}"" id=""{0}"">{1}</textarea>
+                <script type=""text/javascript"">CKEDITOR.replace('{0}');</script>";
 
-        const string tag = @"<script type=""text/javascript"">CKEDITOR.replace('{0}');</script>";
-
-        public static MvcHtmlString CKEditor(this HtmlHelper helper, string id) {
-            return MvcHtmlString.Create(String.Format(tag, id));
+        public static MvcHtmlString CKEditor(this HtmlHelper helper, string id, string content) {
+            if (String.IsNullOrEmpty(content)) {
+                content = "Please enter conent";
+            }
+            return MvcHtmlString.Create(String.Format(tag, id, content));
         }
-
     }
 }
