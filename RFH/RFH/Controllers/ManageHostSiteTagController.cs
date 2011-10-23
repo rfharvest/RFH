1fb41b3d2c4dbefc44fea6beebe74754be1eb959
andrewdothay <andrewcameronhay@hotmail.com>
2011-10-22 23:42:16 -0700

    added tag lookup editor

---
+++ RFH/RFH/Controllers/ManageHostSiteTagController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RFH.Infrastructure;
+using RFH.Models;
+
+namespace RFH.Controllers
+{ 
+    public class ManageHostSiteTagController : Controller
+    {
+        private DataContext _dataContext = new DataContext();
+
+        //
+        // GET: /ManageHostSiteTag/
+
+        public ViewResult Index()
+        {
+            return View(_dataContext.HostSiteTags.ToList());
+        }
+
+        //
+        // GET: /ManageHostSiteTag/Details/5
+
+        public ViewResult Details(int id)
+        {
+            var model = new ManageHostSiteTagDetailViewModel();
+            model.HostSiteTag = _dataContext.HostSiteTags.Find(id);
+            model.HostSiteTagValues = _dataContext.HostSiteTagValues.Where(m => m.HostSiteTagId == id).ToList();
+
+            return View(model);
+        }
+
+        //
+        // GET: /ManageHostSiteTag/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /ManageHostSiteTag/Create
+
+        [HttpPost]
+        public ActionResult Create(HostSiteTag hostsitetag)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataContext.HostSiteTags.Add(hostsitetag);
+                _dataContext.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(hostsitetag);
+        }
+        
+        //
+        // GET: /ManageHostSiteTag/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            HostSiteTag hostsitetag = _dataContext.HostSiteTags.Find(id);
+            return View(hostsitetag);
+        }
+
+        //
+        // POST: /ManageHostSiteTag/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(HostSiteTag hostsitetag)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataContext.Entry(hostsitetag).State = EntityState.Modified;
+                _dataContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hostsitetag);
+        }
+
+        //
+        // GET: /ManageHostSiteTag/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            HostSiteTag hostsitetag = _dataContext.HostSiteTags.Find(id);
+            return View(hostsitetag);
+        }
+
+        //
+        // POST: /ManageHostSiteTag/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            HostSiteTag hostsitetag = _dataContext.HostSiteTags.Find(id);
+            _dataContext.HostSiteTags.Remove(hostsitetag);
+            _dataContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _dataContext.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RFH/RFH/Controllers/ManageHostSiteTagValueController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using RFH.Infrastructure;
+using RFH.Models;
+
+namespace RFH.Controllers
+{ 
+    public class ManageHostSiteTagValueController : Controller
+    {
+        private DataContext _dataContext = new DataContext();
+
+        //
+        // GET: /ManageHostSiteTagValue/Details/5
+
+        public ViewResult Details(int id)
+        {
+            HostSiteTagValue hostsitetagvalue = _dataContext.HostSiteTagValues.Find(id);
+            return View(hostsitetagvalue);
+        }
+
+        //
+        // GET: /ManageHostSiteTagValue/Create
+
+        public ActionResult Create(int id)
+        {
+            var model = new HostSiteTagValue();
+            model.HostSiteTagId = id;
+            return View(model);
+        } 
+
+        //
+        // POST: /ManageHostSiteTagValue/Create
+
+        [HttpPost]
+        public ActionResult Create(HostSiteTagValue hostsitetagvalue)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataContext.HostSiteTagValues.Add(hostsitetagvalue);
+                _dataContext.SaveChanges();
+                return RedirectToAction("Details", "ManageHostSiteTag", new { id=hostsitetagvalue.HostSiteTagId});  
+            }
+
+            return View(hostsitetagvalue);
+        }
+        
+        //
+        // GET: /ManageHostSiteTagValue/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            HostSiteTagValue hostsitetagvalue = _dataContext.HostSiteTagValues.Find(id);
+            return View(hostsitetagvalue);
+        }
+
+        //
+        // POST: /ManageHostSiteTagValue/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, HostSiteTagValue hostsitetagvalue)
+        {
+            var record = _dataContext.HostSiteTagValues.Single(m => m.Id == id);
+
+            if (TryUpdateModel(record))
+            {
+                _dataContext.SaveChanges();
+                return RedirectToAction("Details", "ManageHostSiteTag", new { id=hostsitetagvalue.HostSiteTagId });  
+            }
+
+            return View(hostsitetagvalue);
+        }
+
+        //
+        // GET: /ManageHostSiteTagValue/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            HostSiteTagValue hostsitetagvalue = _dataContext.HostSiteTagValues.Find(id);
+            return View(hostsitetagvalue);
+        }
+
+        //
+        // POST: /ManageHostSiteTagValue/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            HostSiteTagValue hostsitetagvalue = _dataContext.HostSiteTagValues.Find(id);
+            _dataContext.HostSiteTagValues.Remove(hostsitetagvalue);
+            _dataContext.SaveChanges();
+            return RedirectToAction("Details", "ManageHostSiteTag", new { id = hostsitetagvalue.HostSiteTagId });  
+        }
+    }
+}
