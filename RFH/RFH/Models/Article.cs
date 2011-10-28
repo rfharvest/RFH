c46025344cd9bdc404791e3c64f6f64a7d73916e
Andrew Hay <andrewcameronhay@hotmail.com>
2011-10-27 21:12:19 -0700

    making field names consistent

--- RFH/RFH/Models/Article.cs
+++ RFH/RFH/Models/Article.cs
@@ -1,40 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 
-namespace RFH.Models {
-    public class Article {
-
-
-
+namespace RFH.Models 
+{
+    public class Article 
+    {
         public int Id { get; set; }
 
-
-
-        [Required]
-        public string Content { get; set; }
-
-        [Required]
-        public string ShortDescription { get; set; }
-
-        
-
-
+        [DisplayName("Host Site Name")]
         public int HostSiteId { get; set; }
 
         public HostSite HostSite { get; set; }
 
-
+        [DisplayName("Category Name")]
         public int CategoryId { get; set; }
 
         public Category Category { get; set; }
 
-
+        [DisplayName("Is Published")]
         public bool IsPublished { get; set; }
 
+        [DisplayName("Short Description")]
+        [Required]
+        public string ShortDescription { get; set; }
 
-
+        [Required]
+        public string Content { get; set; }
     }
 }
--- RFH/RFH/Models/HostSite.cs
+++ RFH/RFH/Models/HostSite.cs
@@ -1,37 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
 
-namespace RFH.Models {
-    public class HostSite {
-
-        public HostSite() {
-
+namespace RFH.Models 
+{
+    public class HostSite 
+    {
+        public HostSite() 
+        {
             this.MetaData = new List<HostSiteMetaData>();
         }
 
-
         public int Id { get; set; }
 
+        [DisplayName("Host Site Url")]
         public string HostSiteUrl { get; set; }
 
+        [DisplayName("Host Site Name")]
         [Required]
         public string Name { get; set; }
 
         [Required]
         public string Description { get; set; }
 
-
         public string Area { get; set; }
 
-
+        [DisplayName("Is Active")]
         public bool IsActive { get; set; }
 
-
         public ICollection<Article> Articles { get; set; }
-
 
         public ICollection<HostSiteMetaData> MetaData { get; set; }
 
