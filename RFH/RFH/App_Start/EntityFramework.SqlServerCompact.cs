52a4160d6d35fc7479e28bd0fb2e1bb5ac29e126
Andrew Hay <andrewcameronhay@hotmail.com>
2011-10-29 14:56:03 -0700

    added nuget package named EntityFramework.SqlServerCompact

---
+++ RFH/RFH/App_Start/EntityFramework.SqlServerCompact.cs
@@ -0,0 +1,15 @@
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(RFH.App_Start.EntityFramework_SqlServerCompact), "Start")]
+
+namespace RFH.App_Start
+{
+    public static class EntityFramework_SqlServerCompact
+    {
+        public static void Start()
+        {
+            Database.DefaultConnectionFactory = new SqlCeConnectionFactory("System.Data.SqlServerCe.4.0");
+        }
+    }
+}
