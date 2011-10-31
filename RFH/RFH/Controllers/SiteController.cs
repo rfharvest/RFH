653e0521cd1b9450edf93a20c90f4820db950ca6
Andrew Hay <andrewcameronhay@hotmail.com>
2011-10-30 23:06:09 -0700

    added sidebar to /site page

--- RFH/RFH/Controllers/SiteController.cs
+++ RFH/RFH/Controllers/SiteController.cs
@@ -2,6 +2,7 @@
 using System.Data.Entity;
 using System.Web.Mvc;
 using RFH.Infrastructure;
+using RFH.Models;
 
 namespace RFH.Controllers
 {
@@ -12,12 +13,35 @@ namespace RFH.Controllers
         {
             _dataContext.Categories.ToList();
 
-            var model = _dataContext.HostSites
+            var hostSite = _dataContext.HostSites
                             .Include(m => m.Articles)
                             .Where(m => m.UrlFriendlyName == id)
                             .Where(m => m.IsActive)
                             .Single();
-            
+
+            // TODO: Move the following filter execution from C# to the database
+
+            var selectedTagValues = _dataContext.HostSiteToHostSiteTagValues
+                .Where(m => m.HostSiteId == hostSite.Id)
+                .Select(m => m.HostSiteTagValue)
+                .OrderBy(m => m.Name)
+                .ToList();
+
+            var distinctHostSiteTags = selectedTagValues.Select(m => m.HostSiteTagId).Distinct();
+
+            var hostSiteTags = _dataContext.HostSiteTags
+                .Include(m => m.HostSiteTagValues)
+                .ToList()
+                .Where(m => distinctHostSiteTags.Any(d => d == m.Id))
+                .OrderBy(m => m.Name);
+
+            var model = new SiteDetailsViewModel
+                            {
+                                HostSite = hostSite,
+                                HostSiteTags = hostSiteTags,
+                                HostSiteTagValues = selectedTagValues
+                            };
+
             return View(model);
         }
     }
---
+++ RFH/RFH/Models/SiteDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RFH.Models
+{
+    public class SiteDetailsViewModel
+    {
+        public HostSite HostSite { get; set; }
+
+        public IEnumerable<HostSiteTag> HostSiteTags { get; set; }
+
+        public IEnumerable<HostSiteTagValue> HostSiteTagValues { get; set; }
+    }
+}
