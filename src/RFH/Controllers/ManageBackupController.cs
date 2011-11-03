56785e2eeaef6c0fae9aea445bcffee8ae7d05b3
Andrew Hay <andrewcameronhay@hotmail.com>
2011-11-02 21:42:28 -0700

    changed backup to just download from website instead of sending email

---
+++ src/RFH/Controllers/ManageBackupController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using RFH.Services;
+
+namespace RFH.Controllers
+{
+    [Authorize]
+    public class ManageBackupController : Controller
+    {
+        public ActionResult Index()
+        {
+            var files = Directory.GetFiles(AppDataFolder, "*.zip")
+                .Select(m => Path.GetFileName(m));
+
+            return View(files);
+        }
+
+        public FileResult Download(string id)
+        {
+            var filename = GetFileName(id);
+            var fullFileName = Path.Combine(AppDataFolder, filename);
+            return File(fullFileName, "application/zip", Server.UrlEncode(filename));
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var filename = GetFileName(id);
+            return View("Delete", null, filename);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string id, FormCollection form)
+        {
+            var filename = GetFileName(id);
+            var fullFileName = Path.Combine(AppDataFolder, filename);
+
+            if (System.IO.File.Exists(fullFileName))
+            {
+                System.IO.File.Delete(fullFileName);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            try
+            {
+                var rootPath = Server.MapPath("/");
+                var zipFile = string.Format("backup-{0}.zip", DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"));
+                var fullZipFile = Path.Combine(AppDataFolder, zipFile);
+
+                var backupService = new BackupService();
+                backupService.ExecuteBackup(rootPath, fullZipFile);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("_form", ex.ToString());
+                return View();
+            }
+        }
+
+        private string AppDataFolder
+        {
+            get
+            {
+                return Server.MapPath("/App_Data");
+            }
+        }
+
+        private static string GetFileName(string id)
+        {
+            var filename = id + ".zip";
+            return filename;
+        }
+    }
+}
--- src/RFH/Services/BackupService.cs
+++ src/RFH/Services/BackupService.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using RFH.Infrastructure;
 
 namespace RFH.Services
 {
     public class BackupService
     {
         public void ExecuteBackup(string rootPath, string tempZipPath)
-        {
+        {   
             ZipAllFiles(rootPath, tempZipPath);
-            EmailZipFileToAdministrator(tempZipPath);
-            File.Delete(tempZipPath);
         }
 
         private void ZipAllFiles(string rootPath, string tempZipPath)
@@ -20,19 +16,6 @@ namespace RFH.Services
             zipService.OpenZipFile(tempZipPath);
             ZipDirectory(zipService, rootPath, "root");
             zipService.CloseZipFile();
-        }
-
-        private void EmailZipFileToAdministrator(string websiteZipFilePath)
-        {
-            var emailList = new List<string> { "andrewcameronhay@gmail.com" };
-
-            var mailService = new MailService();
-
-            mailService.Send(
-                emailList,
-                "Website Backup",
-                "Hello, \r\n\r\nThe website backup is attached.\r\n\r\nRegards,\r\n\r\nThe Website.",
-                websiteZipFilePath);
         }
 
         private void ZipDirectory(ZipService zipService, string directoryPath, string relativePath)
