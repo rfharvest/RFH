d6457624b3874e22a3b4565d2d3182a244f19b01
Andrew Hay <andrewcameronhay@hotmail.com>
2011-11-12 07:56:47 -0800

    consolidating html and css layout

--- src/RFH/Scripts/custom.js
+++ src/RFH/Scripts/custom.js
@@ -1,11 +1,41 @@
 ﻿/* custom javascript file */
 
-$(function () {
-    var sidebarHeight = $('#sidebar').height();
-    var contentHeight = $('#content').outerHeight();
+var gAdjustLayoutHeightIsQueued = false;
+
+function adjustLayoutHeight() {
+
+    gAdjustLayoutHeightIsQueued = false;
+    
+    var $sidebar = $('#sidebar');
+    var $content = $('#content');
+    var sidebarHeight = $sidebar.outerHeight();
+    var contentHeight = $content.outerHeight();
+
     if (sidebarHeight > contentHeight) {
-        $('#content').css('height', sidebarHeight-40 + 'px');
+        $content.css('height', sidebarHeight - 40 + 'px');
     } else {
-        $('#sidebar').css('height', contentHeight + 'px');
+        $sidebar.css('height', contentHeight + 'px');
     }
+}
+
+function initWindowResizeFeature() {
+    
+    $(window).resize(function () {
+
+        if (!gAdjustLayoutHeightIsQueued) {
+
+            gAdjustLayoutHeightIsQueued = true;
+            window.setTimeout(function () {
+                adjustLayoutHeight();
+            }, 1000);
+        }
+    });
+}
+
+$(function () {
+
+    adjustLayoutHeight();
+
+    initWindowResizeFeature();
+
 });
