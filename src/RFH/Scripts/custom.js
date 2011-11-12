98b9435b67d5626629aff92e1d32a824cdacdd7e
Andrew Hay <andrewcameronhay@hotmail.com>
2011-11-11 22:19:48 -0800

    revising page layout to fix height issue between sidebar and content columns

---
+++ src/RFH/Scripts/custom.js
@@ -0,0 +1,11 @@
+﻿/* custom javascript file */
+
+$(function () {
+    var sidebarHeight = $('#sidebar').height();
+    var contentHeight = $('#content').outerHeight();
+    if (sidebarHeight > contentHeight) {
+        $('#content').css('height', sidebarHeight-40 + 'px');
+    } else {
+        $('#sidebar').css('height', contentHeight + 'px');
+    }
+});
