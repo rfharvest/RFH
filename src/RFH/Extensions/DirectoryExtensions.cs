32a5a0da106ca3c4f92db6d20c469a0a5332dc29
durkc <chris@durkin-consulting.com>
2012-10-20 16:58:52 -0700

    mnb.mnb

---
+++ src/RFH/Extensions/DirectoryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Web;
+
+namespace RFH.Extensions
+{
+    public static class DirectoryExtensions
+    {
+        public static FileInfo GetFileInfo(this string url, HttpServerUtilityBase server)
+        {
+            var physicalPath = server.MapPath(url);
+            var file = new FileInfo(physicalPath);
+            return file;
+        }
+
+        public static DirectoryInfo GetDirectoryInfo(this string folderUrl, HttpServerUtilityBase server)
+        {
+            var folderPhysicalPath = server.MapPath(folderUrl);
+            var directoryInfo = new DirectoryInfo(folderPhysicalPath);
+            return directoryInfo;
+        }
+    }
+}
---
+++ src/RFH/Models/ManageDocumentIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RFH.Models
+{
+    public class ManageDocumentIndexViewModel
+    {
+        public string FolderUrl { get; set; }
+        public IList<string> DocumentUrls { get; set; }
+    }
+}
