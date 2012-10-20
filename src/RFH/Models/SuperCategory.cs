14cf31660f50f9bf970de693d78e7f212ba23ea0
Jonathan Christian <jchristian>
2012-10-20 15:40:47 -0700

    Merge

---
+++ src/RFH/Models/SuperCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RFH.Models
+{
+    public class SuperCategory
+    {
+        [Key]
+        public int SuperCategoryId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string UrlFriendlyName { get; set; }
+
+        public ICollection<Category> Categories { get; set; }
+    }
+}
