b66e67d2039f1cc9ba31649daceecf8d3dffcb0a
Sujith Gowda <sujith.krish@gmail.com>
2012-10-20 17:55:03 -0700

    Added Sidebar files

--- src/RFH/Controllers/HostSiteSidebarController.cs
+++ src/RFH/Controllers/HostSiteSidebarController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RFH.Models;
+using RFH.Infrastructure;
+using System.Data.Entity;
 
 namespace RFH.Controllers
 {
@@ -14,6 +17,54 @@ namespace RFH.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        private DataContext _dataContext = new DataContext();
+
+        public ActionResult Index(int id)
+        {
+            var article = _dataContext.Articles
+                .Include(a => a.HostSite)
+                .Include(a => a.Category)
+                .Where(a => a.IsPublished)
+                .Where(a => a.Id == id)
+                .FirstOrDefault();
+
+            var relatedArticles = _dataContext.Articles
+                .Where(m => m.CategoryId == article.CategoryId)
+                .Where(m => m.Id != article.Id)
+                .Where(m => m.IsPublished)
+                .Select(m => new RelatedArticle
+                {
+                    ArticleId = m.Id,
+                    HostSiteName = m.HostSite.Name
+                }).ToList();
+
+            // TODO: Move the following filter execution from C# to the database
+
+            var selectedTagValues = _dataContext.HostSiteToHostSiteTagValues
+                .Where(m => m.HostSiteId == article.HostSiteId)
+                .Select(m => m.HostSiteTagValue)
+                .OrderBy(m => m.Name)
+                .ToList();
+
+            var distinctHostSiteTags = selectedTagValues.Select(m => m.HostSiteTagId).Distinct();
+
+            var hostSiteTags = _dataContext.HostSiteTags
+                .Include(m => m.HostSiteTagValues)
+                .ToList()
+                .Where(m => distinctHostSiteTags.Any(d => d == m.Id))
+                .OrderBy(m => m.Name);
+
+            var model = new ArticleIndexViewModel
+            {
+                Article = article,
+                RelatedArticles = relatedArticles,
+                HostSiteTags = hostSiteTags,
+                HostSiteTagValues = selectedTagValues
+            };
+
+            return View(model);
         }
 
     }
