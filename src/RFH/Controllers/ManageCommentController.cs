599f87b2318385192360c868e33aabac099199f0
Sujith Gowda <sujith.krish@gmail.com>
2012-10-21 14:43:00 -0700

    Updated manageComments section

--- src/RFH/Controllers/ManageCommentController.cs
+++ src/RFH/Controllers/ManageCommentController.cs
@@ -28,102 +28,40 @@ namespace RFH.Controllers
         public ActionResult Detail(int id)
         {
             var model = new ManageCommentDetailViewModel();
-            model.Comment = _dataContext.Comments.Single(m=>m.CommentId == id);//.Find(model.Comment...Categories.Include(m => m.SuperCategory).Single(m => m.Id == id);
-            //model.SuperCategory = _dataContext.SuperCategories.Find(model.Category.SuperCategoryId);
+            model.Comment = _dataContext.Comments.Single(m=>m.CommentId == id);
 
             return View(model);
         }
 
-        public ActionResult Edit(int id)
-        {
-            var model = new ManageCategoryEditViewModel
-                            {
-                                Category = _dataContext.Categories.Single(m => m.Id == id),
-                                SuperCategories = GetSuperCategoryListItems(_dataContext.SuperCategories.ToList())
-                            };
-
-            return View(model);
-        }
-
-        private IEnumerable<SelectListItem> GetSuperCategoryListItems(List<SuperCategory> items)
-        {
-            return items.Select(x => new SelectListItem
-                                         {
-                                             Text = x.Name,
-                                             Value = x.SuperCategoryId.ToString()
-                                         });
-        }
-
-        private ManageCategoryEditViewModel GetManageCategoryViewModel(Category category)
-        {
-            var model = new ManageCategoryEditViewModel
-                            {
-                                Category = category,
-                                SuperCategories = GetSuperCategoryListItems(_dataContext.SuperCategories.ToList())
-                            };
-
-            return model;
-        }
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection form)
-        {
-            var category = _dataContext.Categories.Single(m => m.Id == id);
-
-            if (TryUpdateModel(category, "Category"))
-            {
-                category.UrlFriendlyName = Regex.Replace(category.Name, @"[^\w]+", "-", RegexOptions.IgnoreCase);
-                _dataContext.SaveChanges();
-                return RedirectToAction("Detail", new { category.Id });
-            }
-
-            var model = GetManageCategoryViewModel(category);
-
-            return View(model);
-        }
 
         public ActionResult Create()
         {
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Create(Category model)
-        {
-            if (ModelState.IsValid)
-            {
-                model.UrlFriendlyName = Regex.Replace(model.Name, @"[^\w]+", "-", RegexOptions.IgnoreCase);
-                _dataContext.Categories.Add(model);
-                _dataContext.SaveChanges();
-
-                return RedirectToAction("Detail", new { model.Id });
-            }
-
-            return View(model);
-        }
 
         public ActionResult Delete(int id)
         {
-            var model = _dataContext.Categories.Single(m => m.Id == id);
+            var model = _dataContext.Comments.Single(m => m.CommentId == id);
             return View(model);
         }
 
         [HttpPost]
         public ActionResult Delete(int id, FormCollection form)
         {
-            Category model = null;
+            Comment model = null;
 
             try 
             {
-                model = _dataContext.Categories.Find(id);
-                _dataContext.Categories.Remove(model);
+                model = _dataContext.Comments.Find(id);
+                _dataContext.Comments.Remove(model);
                 _dataContext.SaveChanges();
 
                 return RedirectToAction("Index");
             }
             catch (DbUpdateException)
             {
-                ModelState.AddModelError("Id", "Unable to delete. Please confirm there are no Articles linked to this category.");
+                ModelState.AddModelError("Id", "Unable to delete. Please verify again if there are comments available in the list.");
                 return View(model);
             }
         }
