b0082cc5c651ac0ac33dd62011496411168d71aa
Sujith Gowda <sujith.krish@gmail.com>
2012-10-20 17:58:19 -0700

    Added HostSiteSidebarIndexViewmodel

---
+++ src/RFH/Models/HostSiteSidebarIndexViewModel.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace RFH.Models 
+{
+    public class HostSiteSidebarIndexViewModel 
+    {
+        public Article Article { get; set; }
+
+        public IEnumerable<RelatedArticle> RelatedArticles { get; set; }
+
+        public IEnumerable<HostSiteTag> HostSiteTags { get; set; }
+
+        public IEnumerable<HostSiteTagValue> HostSiteTagValues { get; set; }
+    }
+}
