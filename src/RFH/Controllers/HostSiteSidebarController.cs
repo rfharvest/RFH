7e36fc316b8b1e5009be2c399a059612837c0f14
Sujith Gowda <sujith.krish@gmail.com>
2012-10-21 01:54:29 -0700

    Added skyscraper display in the right hand side section of page

--- src/RFH/Controllers/HostSiteSidebarController.cs
+++ src/RFH/Controllers/HostSiteSidebarController.cs
@@ -14,38 +14,25 @@ namespace RFH.Controllers
         //
         // GET: /HostSiteSidebar/
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         private DataContext _dataContext = new DataContext();
 
-        public ActionResult Index(int id)
+        public ActionResult HostSiteSidebar(string id)
         {
-            var article = _dataContext.Articles
-                .Include(a => a.HostSite)
-                .Include(a => a.Category)
-                .Where(a => a.IsPublished)
-                .Where(a => a.Id == id)
-                .FirstOrDefault();
+            _dataContext.Categories.ToList();
 
-            var relatedArticles = _dataContext.Articles
-                .Where(m => m.CategoryId == article.CategoryId)
-                .Where(m => m.Id != article.Id)
-                .Where(m => m.IsPublished)
-                .Select(m => new RelatedArticle
-                {
-                    ArticleId = m.Id,
-                    HostSiteName = m.HostSite.Name
-                }).ToList();
+            var hostSite = _dataContext.HostSites
+                            .Include(m => m.Articles)
+                            .Where(m => m.UrlFriendlyName == id)
+                            .Where(m => m.IsActive)
+                            .Single();
 
             // TODO: Move the following filter execution from C# to the database
 
-            var selectedTagValues = _dataContext.HostSiteToHostSiteTagValues
-                .Where(m => m.HostSiteId == article.HostSiteId)
+            /*var selectedTagValues = _dataContext.HostSiteToHostSiteTagValues
+                .Where(m => m.HostSiteId == hostSite.Id)
                 .Select(m => m.HostSiteTagValue)
-                .OrderBy(m => m.Name)
+                .OrderBy(m => m.SortOrder)
+                .ThenBy(m => m.Name)
                 .ToList();
 
             var distinctHostSiteTags = selectedTagValues.Select(m => m.HostSiteTagId).Distinct();
@@ -54,16 +41,18 @@ namespace RFH.Controllers
                 .Include(m => m.HostSiteTagValues)
                 .ToList()
                 .Where(m => distinctHostSiteTags.Any(d => d == m.Id))
-                .OrderBy(m => m.Name);
+                .OrderBy(m => m.SortOrder)
+                .ThenBy(m => m.Name);
+             */
 
-            var model = new ArticleIndexViewModel
+            var model = new HostSiteSidebarIndexViewModel
             {
-                Article = article,
-                RelatedArticles = relatedArticles,
+                HostSiteSidebar = hostSite
+                /*,
                 HostSiteTags = hostSiteTags,
                 HostSiteTagValues = selectedTagValues
+                 */ 
             };
-
             return View(model);
         }
 
--- src/RFH/Models/HostSiteSidebarIndexViewModel.cs
+++ src/RFH/Models/HostSiteSidebarIndexViewModel.cs
@@ -1,15 +1,57 @@
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RFH.Models 
 {
     public class HostSiteSidebarIndexViewModel 
     {
-        public Article Article { get; set; }
+        public HostSite HostSiteSidebar { get; set; }
 
         public IEnumerable<RelatedArticle> RelatedArticles { get; set; }
 
         public IEnumerable<HostSiteTag> HostSiteTags { get; set; }
 
         public IEnumerable<HostSiteTagValue> HostSiteTagValues { get; set; }
+    }
+
+    public class HostSiteSidebar
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Host Site Name")]
+        public int HostSiteId { get; set; }
+
+        public HostSite HostSite { get; set; }
+
+        [DisplayName("Category Name")]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        [DisplayName("Is Published")]
+        public bool IsPublished { get; set; }
+
+        [DisplayName("Title")]
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [DisplayName("Short Description")]
+        [UIHint("TextArea")]
+        [Required]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        [UIHint("HtmlContent")]
+        [MaxLength]
+        public string Content { get; set; }
+
+        [UIHint("Image")]
+        [MaxLength(100)]
+        public string ImageFilename { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+
     }
 }
