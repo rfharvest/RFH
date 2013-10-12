b6b7cfe5f0021a3813cd75cb012dc0b88bdf486b
Chris Kinsman <chris@kinsman.net>
2013-10-12 08:40:38 -0700

    Initial cut of standalone page

---
+++ src/RFH/Controllers/PageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RFH.Infrastructure;
+
+namespace RFH.Controllers
+{
+    public class PageController : Controller
+    {
+
+        private DataContext _dataContext = new DataContext();
+        //
+        // GET: /Page/
+
+        public ActionResult Details(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return Redirect("/");
+            }
+
+            var page = _dataContext.Pages.Include(p => p.Articles).Where(a => a.UrlFriendlyName == id).Single();
+
+            return View(page);
+        }
+    }
+}
---
+++ src/RFH/Models/Page.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RFH.Models
+{
+    public class Page
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Page Name")]
+        [Required]
+        public string Name { get; set; }
+
+        public string UrlFriendlyName { get; set; }
+
+        [UIHint("HtmlContent")]
+        [Required]
+        [MaxLength]
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [DisplayName("Hero FileName")]
+        public string HeroFileName { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+    }
+}
