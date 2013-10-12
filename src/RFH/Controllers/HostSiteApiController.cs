1aaee35fe0b29fdf68f3033105a2a2199b946928
Michael <mikevh@gmail.com>
2013-10-12 11:57:45 -0700

    HostSiteApi controller

---
+++ src/RFH/Controllers/HostSiteApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using RFH.Infrastructure;
+using RFH.Models;
+
+namespace RFH.Controllers
+{
+    public class HostSiteApiController : ApiController
+    {
+        private readonly DataContext _dataContext = new DataContext();
+
+        public IEnumerable<HostSite> Get()
+        {
+            var rv = _dataContext.HostSites;
+            return rv;
+        }
+    }
+}
