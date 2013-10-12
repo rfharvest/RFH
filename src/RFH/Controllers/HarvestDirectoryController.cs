d1a305b9fc8aca6178d74604ff0fdbd7d9ed2c42
Chris Kinsman <chris@kinsman.net>
2013-10-12 16:22:00 -0700

    Initial cut of harvest directory

--- src/RFH/Controllers/HarvestDirectoryController.cs
+++ src/RFH/Controllers/HarvestDirectoryController.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RFH.Infrastructure;
 
 namespace RFH.Controllers
 {
     public class HarvestDirectoryController : Controller
     {
+        private DataContext _dataContext = new DataContext();
         //
         // GET: /HarvestDirectory/
 
         public ActionResult Index()
         {
-            return View();
+            // This is the ide of the real page which is marked inactive and contains the stories
+            var harvestDirectoryPageId = 5;
+
+            var stateArticleMapping = _dataContext.Articles.Where(p => p.PageId == harvestDirectoryPageId).ToList().ToDictionary(a => a.ShortDescription, a=> a.Id);
+
+            var stateJson = Newtonsoft.Json.JsonConvert.SerializeObject(stateArticleMapping);
+
+            return View((object) stateJson);
         }
 
     }
