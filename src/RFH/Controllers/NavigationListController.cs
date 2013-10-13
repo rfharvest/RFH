7e57293bf495ec61ee852d6900c8f741b780fe9d
Chris Kinsman <chris@kinsman.net>
2013-10-12 17:52:10 -0700

    Clean up of menu logic and add Harvest Directory

--- src/RFH/Controllers/NavigationListController.cs
+++ src/RFH/Controllers/NavigationListController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using RFH.Infrastructure;
 using System.Data.Entity;
+using RFH.Models;
 
 namespace RFH.Controllers
 {
@@ -16,7 +17,51 @@ namespace RFH.Controllers
 
         public ActionResult ListCategory()
         {
-           return PartialView(_dataContext.SuperCategories.Include("Categories").Include("Pages").Where(sc => sc.IsActive).OrderBy(c=>c.SortOrder).ToList());
+            List<SuperCategoryNavigationList> model = new List<SuperCategoryNavigationList>();
+
+            var superCategories =
+                _dataContext.SuperCategories.Include("Categories")
+                            .Include("Pages")
+                            .Where(sc => sc.IsActive)
+                            .OrderBy(c => c.SortOrder)
+                            .ToList();
+
+            foreach (var superCategory in superCategories)
+            {
+               var superCategoryNavList = new SuperCategoryNavigationList
+                    {
+                        Name = superCategory.Name
+                    };
+
+                superCategoryNavList.Categories = new List<CategoryNavigationList>();
+                superCategoryNavList.Categories.AddRange(superCategory.Categories.Select(c => new CategoryNavigationList
+                    {
+                        Name = c.Name,
+                        Url = "/Category/" + c.UrlFriendlyName,
+                    }));
+                superCategoryNavList.Categories.AddRange(superCategory.Pages.Where(p=>p.IsActive).Select(p => new CategoryNavigationList
+                    {
+                        Name = p.Name,
+                        Url = "/Page/" + p.UrlFriendlyName,
+                    }));
+
+                if (superCategory.SuperCategoryId == 7)
+                {
+                    superCategoryNavList.Categories.Add(new CategoryNavigationList
+                        {
+                            Name = "Harvest Directory",
+                            Url = "/HarvestDirectory",
+                        });
+                }
+
+                superCategoryNavList.Categories = superCategoryNavList.Categories.OrderBy(c => c.Name).ToList();
+
+                model.Add(superCategoryNavList);
+            }
+
+            
+
+           return PartialView(model);
         }
 
 
